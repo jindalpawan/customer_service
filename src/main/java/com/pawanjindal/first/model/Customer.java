43a974743c384df6a494140f094df145ab72a4a2
jindalpawan <jindalpawan99@gmail.com>
2021-03-11 03:06:56 +0530

    add service func

---
+++ src/main/java/com/pawanjindal/first/model/Customer.java
@@ -0,0 +1,44 @@
+package com.pawanjindal.first.model;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class Customer {
+    private int customerId;
+    private String customerFirstName;
+    private String customerLastName;
+    private String customerEmail;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+}
